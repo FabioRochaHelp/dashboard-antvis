159636ad15a7266e46dab7aef41411fc53e5b573
Fabio Rocha <fabiorochahelp@gmail.com>
2021-12-24 12:34:11 -0300

    Monitor Quasar

--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -1,7 +1,92 @@
 
 <template>
-  <div>
-    <div class="row">
+    <q-layout view="hHh lpR fFf">
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <div class="row q-mt-md">
+        <div class="col-md-12">
+            <div class="justify-center q-pa-md q-gutter-sm">
+              <q-card class="my-card q-mt-xl">
+                <q-card-section class="bg-dark text-white">
+                  <div class="text-h6">{{title}}</div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="justify-center q-pa-md q-gutter-sm">
+              <q-card v-for="call in onHoldCalls" v-bind:key="call" class="my-card">
+                <q-card-section class="bg-warning text-dark">
+                  <div class="text-subtitle2">{{call.clid}}</div>
+                </q-card-section>
+              </q-card>
+            </div>
+        </div>
+    </div>
+
+    </q-drawer>
+
+    <q-page-container>
+
+         <div class="row justify-center q-gutter-md">
+            <q-card v-for="agent in agents" v-bind:key="agent" class="my-card col-md-3">
+              <q-card-section class="bg-primary text-white">
+                <div class="text-h6">{{agent.name}}</div>
+                <div class="text-subtitle2">{{agent.name}}</div>
+              </q-card-section>
+
+              <q-separator />
+
+              <q-card-actions align="right">
+                <q-btn flat>{{agent.calleridname}}</q-btn>
+                <q-btn flat>05:00</q-btn>
+              </q-card-actions>
+            </q-card>
+         </div>
+         <div class="row q-gutter-md q-mt-sm">
+         <div class="col">
+            <q-banner rounded class="bg-red-8 text-white">
+
+              Chamada em espera
+
+              <div>
+                {{ onHoldCalls.length }}
+              </div>
+            </q-banner>
+         </div>
+         <div class="col">
+            <q-banner rounded class="bg-green-8 text-white">
+
+              Atendidas
+
+              <div>
+              {{ answered }}
+              </div>
+            </q-banner>
+         </div>
+         <div class="col">
+            <q-banner rounded class="bg-orange-8 text-white">
+
+               Abandonadas
+
+              <div>
+                  {{ abandon }}
+              </div>
+            </q-banner>
+         </div>
+         <div class="col">
+            <q-banner rounded class="bg-purple-8 text-white">
+
+                  Total
+
+                <div>
+                  {{ (parseInt(answered)+parseInt(abandon)) }}
+                  </div>
+             </q-banner>
+          </div>
+        </div>
+    </q-page-container>
+  </q-layout>
+
+  <!-- <div>
+    <div class="col-4">
       <div class="col-12">
         <h1><strong>Fila: </strong>{{title}}</h1>
       </div>
@@ -69,13 +154,13 @@
         </table>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
 /* eslint-disable */
-const io = require('socket.io-client')
-// import  io  from '../../socket.io-client/dist/socket.io'
+import socket from '../socket/io'
+
 export default {
   name: 'Monitor',
   data () {
@@ -91,8 +176,6 @@ export default {
     setTimeout(function () {
       fetch('http://localhost:3000/stats?queue=Entrada')
     }, 1000)
-
-    const socket = io('http://localhost:3000')
 
     socket.on('Entrada', async (event) => {
       console.log(JSON.stringify(event))
@@ -127,15 +210,13 @@ export default {
         const tmpAgents = []
 
         this.agents.forEach((agent) => {
-          if (agent.location == event.interface) {
-            agent.status = event.status
-            agent.paused = event.paused
-          }
+            agent.paused = event.paused;
+            agent.calleridname = event.calleridname;
+            agent.status = event.status;
+            tmpAgents.push(agent);
+        });
 
-          tmpAgents.push(agent)
-        })
-
-        this.agents = tmpAgents
+        this.agents = tmpAgents;
       }
 
       if (event.event == 'QueueCallerAbandon') {
@@ -143,7 +224,17 @@ export default {
       }
 
       if (event.event == 'AgentConnect') {
-        this.answered = parseInt(this.answered) + 1
+        let tmpAgents = [];
+
+        this.agents.forEach((agent) => {
+            agent.paused = event.paused;
+            agent.calleridname = event.calleridname;
+            agent.status = event.status;
+            tmpAgents.push(agent);
+        });
+
+        this.agents = tmpAgents;
+        this.answered = parseInt(this.answered)+1;
       }
     })
   }
